920737d1f80c0e73e1c582da6f5b4d80c149411e
jiwoo <sgo8308@gmail.com>
2023-03-26 15:46:42 +0900

    LineRepository findAll 메소드에 fetch join 적용

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -1,9 +1,12 @@
 package nextstep.subway.line.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface LineRepository extends JpaRepository<Line, Long> {
+
+    @Override
+    @Query("select distinct l from Line l join fetch l.sections s join fetch s.upStation join fetch s.downStation")
+    List<Line> findAll();
 }
